cca85986573cd030f0bf991e42cef16f3dafe0e9
Egor Khilchuk <egor.khilchuk@gmail.com>
2022-04-12 16:48:27 +0300

    routes added

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+const Router = require('express')
+const router = new Router()
+const userRouter = require('./userRouter')
+const subjectRouter = require('./subjectRouter')
+const testRouter = require('./testRouter')
+const testQuestionRouter = require('./testQuestionRouter')
+const testAnswerRouter = require('./testAnswerRouter')
+const testSessionRouter = require('./testSessionRouter')
+const testSessionResultRouter = require('./testSessionResultRouter')
+
+router.use('/user', userRouter)
+router.use('/subject', subjectRouter)
+router.use('/test', testRouter)
+router.use('/testQuestion', testQuestionRouter)
+router.use('/testAnswer', testAnswerRouter)
+router.use('/testSession',testSessionRouter)
+router.use('/TestSessionResult',testSessionResultRouter)
+
+module.exports = router
